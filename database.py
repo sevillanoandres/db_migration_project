18fa3316836c77a48be9e008920495dd267b0c07
Oscar Sevillano <osevillano@proserveit.com>
2024-07-22 01:25:38 -0400

    changing database

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def init_db(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employeeapichallenge.db'  # or any other SQL database URI
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://apichallengeadmin:Oscar1984$@srvapichallenge.database.windows.net/API_Challenge_DB?driver=ODBC+Driver+18+for+SQL+Server'  # or any other SQL database URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     with app.app_context():
